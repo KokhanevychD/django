b6438166ed63c3b0dd33c9183c7d2de4d138a001
Dmytro <sick.rifel@gmail.com>
2020-06-16 03:06:51 +0300

    ajax api calls for filtered articles with query params

--- blog/static/ajax/ajax.js
+++ blog/static/ajax/ajax.js
@@ -1,4 +1,5 @@
-function ajax_load(url='/api/articles'){
+let basic_url = '/api/articles'
+function ajax_load(url=basic_url){
 
     $.ajax({
         type:'GET',
@@ -9,11 +10,16 @@ function ajax_load(url='/api/articles'){
             container.find('.tagsdiv').empty();
             $('.articlelist').empty();
 
-            if (data.next != false){
+            if (data.next != null){
                 $('.next').attr('href', data.next).css('display', '')
+            }else{
+                $('.next').css('display', 'none')
             };
-            if (data.previous != false){
+
+            if (data.previous != null){
                 $('.prev').attr('href', data.previous).css('display', '')
+            }else{
+                $('.prev').css('display', 'none')
             };
 
             for (idx in data.results) {
@@ -25,13 +31,13 @@ function ajax_load(url='/api/articles'){
                     item = current_data.tags[idx];
                     let new_tag = tag.clone();
                     new_tag.find('.badge').text(item.name);
-                    tag_url='posts/tags/' + item.name;
+                    tag_url= basic_url + '?tags=' + item.name;
                     new_tag.attr('href', tag_url);
                     new_tag.appendTo(clone.find('.tagsdiv'));
                 };
 
                 clone.find('.content').text(current_data.content);
-                author_url = '/posts/' + current_data.author.username;
+                author_url = basic_url + '?author=' + current_data.author.username;
                 clone.find('.author').text(current_data.author.username);
                 clone.find('.author').attr('href', author_url)
                 clone.find('.postdate').text(current_data.post_date);
@@ -46,10 +52,9 @@ function ajax_load(url='/api/articles'){
 
 $('#listbutton').click(function(){
     ajax_load()
-})
-
-$('.next, .prev').click(function(event){
+});
+$(document).on('click','.next, .prev, .author, .tag_link', function(event){
     event.preventDefault();
     let page_url = $(this).attr('href');
     ajax_load(page_url);
-})
+});
